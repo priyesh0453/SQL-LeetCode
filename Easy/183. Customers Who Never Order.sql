62413648f212f770a9f8a27dd69a9161502e3db7
Priyesh Mishra <priyeshbitsh@gmail.com>
2023-02-04 02:42:07 +0530

    Initial Commit

---
+++ Easy/183. Customers Who Never Order.sql
@@ -0,0 +1,63 @@
+/*
+Table: Customers
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table indicates the ID and name of a customer.
+
+
+Required Query:
+
+Report all customers who never order anything.
+
+
+SQL Schema:
+
+Create table If Not Exists Customers (id int, name varchar(255))
+Create table If Not Exists Orders (id int, customerId int)
+Truncate table Customers
+insert into Customers (id, name) values ('1', 'Joe')
+insert into Customers (id, name) values ('2', 'Henry')
+insert into Customers (id, name) values ('3', 'Sam')
+insert into Customers (id, name) values ('4', 'Max')
+Truncate table Orders
+insert into Orders (id, customerId) values ('1', '3')
+insert into Orders (id, customerId) values ('2', '1')
+
+Example:
+
+Input: 
+Customers table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Orders table:
++----+------------+
+| id | customerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+Output: 
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+*/
+
+-- Solution:
+SELECT name AS Customers
+FROM Customers LEFT JOIN Orders ON Customers.id = Orders.customerId
+WHERE customerID is NULL
