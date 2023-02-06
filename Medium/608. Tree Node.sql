e55c8fd0ae054241cc84be8b131b68135f684f5b
Priyesh Mishra <priyeshbitsh@gmail.com>
2023-02-07 00:04:24 +0530

    Initial Commit

---
+++ Medium/608. Tree Node.sql
@@ -0,0 +1,64 @@
+/*
+Table: Tree
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| p_id        | int  |
++-------------+------+
+id is the primary key column for this table.
+Each row of this table contains information about the id of a node and the id of its parent node in a tree.
+The given structure is always a valid tree.
+
+
+Required Query:
+
+1) Report the type of each node in the tree.
+
+
+SQL Schema:
+
+Create table If Not Exists Tree (id int, p_id int)
+Truncate table Tree
+insert into Tree (id, p_id) values ('1', 'None')
+insert into Tree (id, p_id) values ('2', '1')
+insert into Tree (id, p_id) values ('3', '1')
+insert into Tree (id, p_id) values ('4', '2')
+insert into Tree (id, p_id) values ('5', '2')
+
+
+Example:
+
+Input: 
+Tree table:
++----+------+
+| id | p_id |
++----+------+
+| 1  | null |
+| 2  | 1    |
+| 3  | 1    |
+| 4  | 2    |
+| 5  | 2    |
++----+------+
+Output: 
++----+-------+
+| id | type  |
++----+-------+
+| 1  | Root  |
+| 2  | Inner |
+| 3  | Leaf  |
+| 4  | Leaf  |
+| 5  | Leaf  |
++----+-------+
+*/
+
+-- Solution:
+SELECT id,
+CASE 
+WHEN p_id IS NULL THEN 'Root'
+WHEN id IN (SELECT DISTINCT(p_id) FROM Tree) THEN 'Inner'
+ELSE 'Leaf' 
+END
+AS type 
+FROM Tree
