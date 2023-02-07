91839e4f7f52111e95f96a0fc0af43d8a353f5f4
Priyesh Mishra <priyeshbitsh@gmail.com>
2023-02-08 00:54:53 +0530

    Initial Commit

---
+++ Easy/197. Rising Temperature.sql
@@ -0,0 +1,54 @@
+/*
+Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the primary key for this table.
+This table contains information about the temperature on a certain day.
+
+
+Required Query:
+
+1) Find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+
+
+SQL Schema:
+
+Create table If Not Exists Weather (id int, recordDate date, temperature int)
+Truncate table Weather
+insert into Weather (id, recordDate, temperature) values ('1', '2015-01-01', '10')
+insert into Weather (id, recordDate, temperature) values ('2', '2015-01-02', '25')
+insert into Weather (id, recordDate, temperature) values ('3', '2015-01-03', '20')
+insert into Weather (id, recordDate, temperature) values ('4', '2015-01-04', '30')
+
+Example:
+
+Input: 
+Weather table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+Output: 
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+*/
+
+-- Solution:
+SELECT W2.id
+FROM Weather W1 JOIN Weather W2
+ON DATEDIFF(W2.recordDate, W1.recordDate) = 1
+WHERE W2.temperature > W1.temperature
