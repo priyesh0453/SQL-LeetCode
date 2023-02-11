7b8dd508c120dcd976dc373e6f1b0265261af644
Priyesh Mishra <priyeshbitsh@gmail.com>
2023-02-11 19:14:07 +0530

    Initial Commit

---
+++ Easy/586. Customer Placing the Largest Number of Orders.sql
@@ -0,0 +1,54 @@
+/*
+Table: Orders
+
++-----------------+----------+
+| Column Name     | Type     |
++-----------------+----------+
+| order_number    | int      |
+| customer_number | int      |
++-----------------+----------+
+order_number is the primary key for this table.
+This table contains information about the order ID and the customer ID.
+
+
+Required Query:
+
+1) Find the customer_number for the customer who has placed the largest number of orders.
+
+
+SQL Schema:
+
+Create table If Not Exists orders (order_number int, customer_number int)
+Truncate table orders
+insert into orders (order_number, customer_number) values ('1', '1')
+insert into orders (order_number, customer_number) values ('2', '2')
+insert into orders (order_number, customer_number) values ('3', '3')
+insert into orders (order_number, customer_number) values ('4', '3')
+
+
+Example:
+
+Input: 
+Orders table:
++--------------+-----------------+
+| order_number | customer_number |
++--------------+-----------------+
+| 1            | 1               |
+| 2            | 2               |
+| 3            | 3               |
+| 4            | 3               |
++--------------+-----------------+
+Output: 
++-----------------+
+| customer_number |
++-----------------+
+| 3               |
++-----------------+
+*/  
+
+--Solution:
+SELECT customer_number
+FROM Orders
+GROUP BY customer_number
+ORDER BY COUNT(DISTINCT(order_number)) DESC
+LIMIT 1
