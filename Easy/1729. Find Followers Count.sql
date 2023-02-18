b75f2f060e8a87042b385c4e6ac69da07235dbb4
Priyesh Mishra <priyeshbitsh@gmail.com>
2023-02-19 00:12:36 +0530

    Initial Commit

---
+++ Easy/1729. Find Followers Count.sql
@@ -0,0 +1,57 @@
+/*
+Table: Followers
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| user_id     | int  |
+| follower_id | int  |
++-------------+------+
+(user_id, follower_id) is the primary key for this table.
+This table contains the IDs of a user and a follower in a social media app where the follower follows the user.
+
+
+Required Query:
+
+1) For each user, return the number of followers.
+
+2) Return the result table ordered by user_id in ascending order.
+
+
+SQL Schema:
+
+Create table If Not Exists Followers(user_id int, follower_id int)
+Truncate table Followers
+insert into Followers (user_id, follower_id) values ('0', '1')
+insert into Followers (user_id, follower_id) values ('1', '0')
+insert into Followers (user_id, follower_id) values ('2', '0')
+insert into Followers (user_id, follower_id) values ('2', '1')
+
+
+Example:
+
+Input: 
+Followers table:
++---------+-------------+
+| user_id | follower_id |
++---------+-------------+
+| 0       | 1           |
+| 1       | 0           |
+| 2       | 0           |
+| 2       | 1           |
++---------+-------------+
+Output: 
++---------+----------------+
+| user_id | followers_count|
++---------+----------------+
+| 0       | 1              |
+| 1       | 1              |
+| 2       | 2              |
++---------+----------------+
+*/  
+
+--Solution:
+SELECT user_id, COUNT(DISTINCT(follower_id)) AS 'followers_count'
+FROM Followers
+GROUP BY user_id
+ORDER BY user_id
