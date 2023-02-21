71e07df095cfb9ec6bb9d2a745025ea79b5daa4e
Priyesh Mishra <priyeshbitsh@gmail.com>
2023-02-22 01:44:16 +0530

    Create Leetcode 1795. Rearrange Products Table

---
+++ Easy/1795. Rearrange Products Table.sql
@@ -0,0 +1,63 @@
+/*
+Table: Products
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| product_id  | int     |
+| store1      | int     |
+| store2      | int     |
+| store3      | int     |
++-------------+---------+
+product_id is the primary key for this table.
+Each row in this table indicates the product's price in 3 different stores: store1, store2, and store3.
+If the product is not available in a store, the price will be null in that store's column.
+
+
+Required Query:
+
+1) Rearrange the Products table so that each row has (product_id, store, price). If a product is not available in a store, do not include a row with that product_id and store combination in the result table.
+
+
+SQL Schema:
+
+Create table If Not Exists Products (product_id int, store1 int, store2 int, store3 int)
+Truncate table Products
+insert into Products (product_id, store1, store2, store3) values ('0', '95', '100', '105')
+insert into Products (product_id, store1, store2, store3) values ('1', '70', 'None', '80')
+
+
+Example:
+
+Input: 
+Products table:
++------------+--------+--------+--------+
+| product_id | store1 | store2 | store3 |
++------------+--------+--------+--------+
+| 0          | 95     | 100    | 105    |
+| 1          | 70     | null   | 80     |
++------------+--------+--------+--------+
+Output: 
++------------+--------+-------+
+| product_id | store  | price |
++------------+--------+-------+
+| 0          | store1 | 95    |
+| 0          | store2 | 100   |
+| 0          | store3 | 105   |
+| 1          | store1 | 70    |
+| 1          | store3 | 80    |
++------------+--------+-------+
+*/
+
+-- Solution:
+SELECT product_id, 'store1' AS 'store', store1 AS price
+FROM Products
+WHERE store1 IS NOT NULL
+UNION
+SELECT product_id, 'store2' AS 'store', store2 AS price
+FROM Products
+WHERE store2 IS NOT NULL
+UNION
+SELECT product_id, 'store3' AS 'store', store3 AS price
+FROM Products
+WHERE store3 IS NOT NULL
