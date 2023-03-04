6a0c1a6c1f88fbe91f9117c4f17ff23717968a3b
Priyesh Mishra <priyeshbitsh@gmail.com>
2023-03-05 00:26:56 +0530

    Create LeetCode 182. Duplicate Emails

---
+++ Easy/182. Duplicate Emails.sql
@@ -0,0 +1,51 @@
+/*
+Table: Person
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table contains an email. The emails will not contain uppercase letters.
+
+
+Required Query:
+
+Report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.
+
+
+SQL Schema:
+
+Create table If Not Exists Person (id int, email varchar(255))
+Truncate table Person
+insert into Person (id, email) values ('1', 'a@b.com')
+insert into Person (id, email) values ('2', 'c@d.com')
+insert into Person (id, email) values ('3', 'a@b.com')
+
+
+Example:
+
+Input: 
+Person table:
++----+---------+
+| id | email   |
++----+---------+
+| 1  | a@b.com |
+| 2  | c@d.com |
+| 3  | a@b.com |
++----+---------+
+Output: 
++---------+
+| Email   |
++---------+
+| a@b.com |
++---------+
+*/
+
+-- Solution:
+SELECT email
+FROM Person
+GROUP BY email
+HAVING COUNT(email) > 1
