1ef609b0cbecd61cde6434c0dd273dc1d2ae8f9b
Priyesh Mishra <priyeshbitsh@gmail.com>
2023-03-09 00:17:12 +0530

    Create LeetCode 1050. Actors and Directors Who Cooperated At Least Three Times

---
+++ Easy/1050. Actors and Directors Who Cooperated At Least Three Times.sql
@@ -0,0 +1,59 @@
+/*
+Table: ActorDirector
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| actor_id    | int     |
+| director_id | int     |
+| timestamp   | int     |
++-------------+---------+
+timestamp is the primary key column for this table.
+
+
+Required Query:
+
+Report that provides the pairs (actor_id, director_id) where the actor has cooperated with the director at least three times.
+
+
+SQL Schema:
+
+Create table If Not Exists ActorDirector (actor_id int, director_id int, timestamp int)
+Truncate table ActorDirector
+insert into ActorDirector (actor_id, director_id, timestamp) values ('1', '1', '0')
+insert into ActorDirector (actor_id, director_id, timestamp) values ('1', '1', '1')
+insert into ActorDirector (actor_id, director_id, timestamp) values ('1', '1', '2')
+insert into ActorDirector (actor_id, director_id, timestamp) values ('1', '2', '3')
+insert into ActorDirector (actor_id, director_id, timestamp) values ('1', '2', '4')
+insert into ActorDirector (actor_id, director_id, timestamp) values ('2', '1', '5')
+insert into ActorDirector (actor_id, director_id, timestamp) values ('2', '1', '6')
+
+
+Example:
+
+Input: 
+ActorDirector table:
++-------------+-------------+-------------+
+| actor_id    | director_id | timestamp   |
++-------------+-------------+-------------+
+| 1           | 1           | 0           |
+| 1           | 1           | 1           |
+| 1           | 1           | 2           |
+| 1           | 2           | 3           |
+| 1           | 2           | 4           |
+| 2           | 1           | 5           |
+| 2           | 1           | 6           |
++-------------+-------------+-------------+
+Output: 
++-------------+-------------+
+| actor_id    | director_id |
++-------------+-------------+
+| 1           | 1           |
++-------------+-------------+
+*/
+
+-- Solution:
+SELECT actor_id, director_id
+FROM ActorDirector
+GROUP BY actor_id, director_id
+HAVING COUNT(*) > 2
